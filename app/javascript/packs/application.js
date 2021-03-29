233080ceef067784d8ceec85367c1cde0864e56e
peachbranch777 <peachbranch.0517@gmail.com>
2021-03-29 20:57:34 +0900

    highcharsカラー変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,6 @@ require("../index")
 require("chart.js")
 require("chartkick").use(require("highcharts"))
 
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
